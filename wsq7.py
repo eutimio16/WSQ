6321c7cc687cfc2af287cef22a9f4e03179b1739
eutimio16 <eutimio@live.com.mx>
2015-03-05 10:13:23 -0600

    Revert "Revert "WSQS""
    This reverts commit acc2cc0e42c060c42be35e4d952811aaf8a72904.

---
+++ wsq7.py
@@ -0,0 +1,4 @@
+print(" este programa te pedira 2 numeros y sumara los numeros que esten en ese rango")#Eutimio MAchuca Parra A01630244
+x=int(input(" inserta el numero del cual empezara la suma"))
+y=int(input(" insrta el numero al cual llegara la suma"))
+print(sum(range(x,y+1)))
